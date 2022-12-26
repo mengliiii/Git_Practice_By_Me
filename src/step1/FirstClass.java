e77dcd7a3fa2aa0acafdc83e1b0d560ed1e98cbd
mengliiii <menglivy@hotmail.com>
2022-12-26 15:20:28 -0500

    create step1 package, FirstClass file

---
+++ src/step1/FirstClass.java
@@ -0,0 +1,13 @@
+package step1;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        /*
+        1-create a project
+        2-enable VCS
+        3-Create gitignore file
+        4-create package and file
+         */
+    }
+}
