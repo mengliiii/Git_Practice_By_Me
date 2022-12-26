76fe52b0d25150c955c01f8dcfda10e8cfa5b12f
mengliiii <menglivy@hotmail.com>
2022-12-26 15:24:12 -0500

    create step2 package, SecondClass

---
+++ src/step2/SecondClass.java
@@ -0,0 +1,16 @@
+package step2;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Git branching");
+        /*
+        -Create a new branch named "tester"
+        -Checkout the local "tester" branch
+        -Create a package named step2
+        -Create a class named SecondClass under the package step2 and print "Git Branching"
+        -Push the new package to the branch named "tester"
+        -Create a pull request to merge the changes to the main/master branch
+        -Go to your GitHub repo and accept the pull request
+         */
+    }
+}
